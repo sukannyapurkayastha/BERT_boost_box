b5aa33e36e8b7da5c0c8072a3f691ade9f5be726
sukannyapurkayastha <purkayasthasukannya020@gmail.com>
2021-01-20 11:43:32 +0530

    Added Source Codes

---
+++ src/main.py
@@ -0,0 +1,53 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+import config
+import dataset
+import torch
+import h5py
+import pandas as pd
+import model_class
+from train_test_files import train, valid
+from torch.utils.data import DataLoader
+
+def run(train_dataset, test_dataset):
+    df_train = pd.read_csv(train_dataset, sep='\t', names=['text', 'relation', 'relation_label'])
+    df_test = pd.read_csv(test_dataset, sep='\t', names=['text', 'relation', 'relation_label'])
+
+    with h5py.File('../data/train_mask.h5', 'r') as hf:
+        data_train = hf['train_mask'][:].tolist()
+    # with h5py.File('../data/test_mask.h5', 'r') as hf:
+     #   data_test = hf['test_mask'][:].tolist()
+
+    train_data_set = dataset.BERT_KBQA_Dataloader(df_train.text.values, df_train.relation_label.values, data_train)
+    #test_data_set = dataset.BERT_KBQA_Dataloader(df_test.text.values, df_train.relation_label.values, data_test)
+
+    train_data_loader = DataLoader(train_data_set, batch_size=config.TRAIN_BATCH_SIZE)
+    # test_data_loader = DataLoader(test_data_set, batch_size=config.VALID_BATCH_SIZE)
+
+    model = model_class.Bert_Kbqa_Model()
+    optimizer = torch.optim.Adam(model.parameters(), lr=3e-5)
+    for epoch in range(config.EPOCHS):
+        train(model, epoch, train_data_loader, optimizer)
+
+    del data_train
+    del train_data_loader
+    del train_data_set
+
+    with h5py.File('../data/test_mask.h5','r') as hf:
+        data_test = hf['test_mask'][:].tolist()
+    test_data_set = dataset.BERT_KBQA_Dataloader(df_test.text.values, df_test.relation_label.values, data_test)
+    test_data_loader = DataLoader(test_data_set, batch_size=config.VALID_BATCH_SIZE,)
+    acc = valid(model, test_data_loader)
+    print("Accuracy on test data = %0.2f%%" % acc)
+
+
+if __name__ == '__main__':
+    train_dataset = '../data/train_data_final.txt'
+    test_dataset = '../data/test_data_final.txt'
+    run(train_dataset, test_dataset)
+
+
---
+++ src/train_test_files.py
@@ -0,0 +1,98 @@
+import torch
+from torch import cuda
+device = 'cuda' if cuda.is_available() else 'cpu'
+#optimizer = torch.optim.Adam(lr=3e-5)
+alpha = 0.5
+
+def calcuate_accu(big_idx, targets):
+    n_correct = (big_idx == targets).sum().item()
+    return n_correct
+
+
+loss_function = torch.nn.CrossEntropyLoss()
+
+
+def mask_loss(outputs, target_mask):
+    return torch.nn.BCEWithLogitsLoss()(outputs, target_mask)
+
+
+def train(model, epoch, training_loader, optimizer):
+    tr_loss = 0
+    n_correct = 0
+    nb_tr_steps = 0
+    nb_tr_examples = 0
+    model.train()
+    for _, data in enumerate(training_loader, 0):
+        ids = data['ids'].to(device, dtype=torch.long)
+        mask = data['mask'].to(device, dtype=torch.long)
+        targets = data['targets'].to(device, dtype=torch.long)
+        mask_labels = data['mask_labels'].to(device, dtype=torch.long)
+
+        outputs = model(ids, mask)
+        # predictions = torch.nn.Softmax(outputs)
+        Loss_CE = loss_function(outputs, targets)
+        Loss_Mask = mask_loss(outputs, mask_labels)
+        loss = Loss_CE + alpha * Loss_Mask
+        tr_loss += Loss_CE.item()
+        big_val, big_idx = torch.max(outputs.data, dim=1)
+        n_correct += calcuate_accu(big_idx, targets)
+
+        nb_tr_steps += 1
+        nb_tr_examples += targets.size(0)
+
+        if _ % 5000 == 0:
+            loss_step = tr_loss / nb_tr_steps
+            accu_step = (n_correct * 100) / nb_tr_examples
+            print(f"Training Loss per 5000 steps: {loss_step}")
+            print(f"Training Accuracy per 5000 steps: {accu_step}")
+
+        optimizer.zero_grad()
+        loss.backward()
+        # # When using GPU
+        optimizer.step()
+
+    print(f'The Total Accuracy for Epoch {epoch}: {(n_correct * 100) / nb_tr_examples}')
+    epoch_loss = tr_loss / nb_tr_steps
+    epoch_accu = (n_correct * 100) / nb_tr_examples
+    print(f"Training Loss Epoch: {epoch_loss}")
+    print(f"Training Accuracy Epoch: {epoch_accu}")
+
+    return
+
+
+def valid(model, testing_loader):
+    model.eval()
+    n_correct = 0
+    tr_loss = 0
+    n_correct = 0
+    nb_tr_steps = 0
+    nb_tr_examples = 0
+    n_wrong = 0
+    total = 0
+    with torch.no_grad():
+        for _, data in enumerate(testing_loader, 0):
+            ids = data['ids'].to(device, dtype=torch.long)
+            mask = data['mask'].to(device, dtype=torch.long)
+            targets = data['targets'].to(device, dtype=torch.long)
+            outputs = model(ids, mask).squeeze()
+            mask_labels = data['mask_labels'].to(device, dtype=torch.long)
+            outputs_ = outputs * mask_labels
+            loss = loss_function(outputs, targets)
+            tr_loss += loss.item()
+            big_val, big_idx = torch.max(outputs.data, dim=1)
+            n_correct += calcuate_accu(big_idx, targets)
+
+            nb_tr_steps += 1
+            nb_tr_examples += targets.size(0)
+
+            if _ % 5000 == 0:
+                loss_step = tr_loss / nb_tr_steps
+                accu_step = (n_correct * 100) / nb_tr_examples
+                print(f"Validation Loss per 100 steps: {loss_step}")
+                print(f"Validation Accuracy per 100 steps: {accu_step}")
+    epoch_loss = tr_loss / nb_tr_steps
+    epoch_accu = (n_correct * 100) / nb_tr_examples
+    print(f"Validation Loss Epoch: {epoch_loss}")
+    print(f"Validation Accuracy Epoch: {epoch_accu}")
+
+    return epoch_accu
